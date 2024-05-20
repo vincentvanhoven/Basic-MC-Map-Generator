a9ac10173b3134fc4a4b75435c4242662af5848e
Vincent van Hoven <11269255+vincentvanhoven@users.noreply.github.com>
2024-05-20 19:39:58 +0200

    [FIX] build.sh bash syntax

--- build_all.sh
+++ build_all.sh
@@ -1,39 +1,39 @@
-ECHO ======== Linux builds ========
-ECHO -n "Building linux/amd64...   "
+echo ======== Linux builds ========
+echo -n "Building linux/amd64...   "
 GOOS=linux
 GOARCH=amd64
 go build -o bin/amd64-linux
-ECHO done
+echo done
 
-ECHO -n "Building linux/386...     "
+echo -n "Building linux/386...     "
 GOOS=linux
 GOARCH=386
 go build -o bin/386-linux
-ECHO done
+echo done
 
 
-ECHO ======= Windows builds =======
-ECHO -n "Building windows/amd64... "
+echo ======= Windows builds =======
+echo -n "Building windows/amd64... "
 GOOS=windows
 GOARCH=amd64
 go build -o bin/amd64-windows.exe
-ECHO done
+echo done
 
-ECHO -n "Building windows/386...   "
+echo -n "Building windows/386...   "
 GOOS=windows
 GOARCH=386
 go build -o bin/386-windows.exe
-ECHO done
+echo done
 
-ECHO ======== macOS builds ========
-ECHO -n "Building macOS/amd64...   "
+echo ======== macOS builds ========
+echo -n "Building macOS/amd64...   "
 GOOS=darwin
 GOARCH=amd64
 go build -o bin/amd64-macos
-ECHO done
+echo done
 
-ECHO -n "Building macOS/arm64...   "
+echo -n "Building macOS/arm64...   "
 GOOS=darwin
 GOARCH=arm64
 go build -o bin/arm64-macos
-ECHO done
+echo done
