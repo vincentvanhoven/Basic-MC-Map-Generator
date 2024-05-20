d27bc60959eec87991fcfdeffae556872e69c021
Vincent van Hoven <11269255+vincentvanhoven@users.noreply.github.com>
2024-05-20 18:28:19 +0200

    [REMOVE] Old implementation

--- Chunk.py
+++
@@ -1,8 +0,0 @@
-class Chunk:
-    def __init__(self, dataOffset, biome = None, timestamp = None, isLoaded = False, x = 0, z = 0):
-        self.dataOffset = dataOffset
-        self.timestamp = timestamp
-        self.isLoaded = isLoaded
-        self.x = x
-        self.z = z
-        self.biome = biome
